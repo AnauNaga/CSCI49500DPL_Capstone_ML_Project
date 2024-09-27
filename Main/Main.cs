e95702460e109a0598b82637a5b1f04f39ddcb84
ethanjensen08@gmail.com <ethanjensen08@gmail.com>
2024-09-26 21:10:40 -0400

    Add project files.

---
+++ Main/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Main
+{
+    public static class Master
+    {
+        public static void Main(string[] args)
+        {
+            string affixes = File.ReadAllText("C:\\Users\\Anau Naga\\source\\repos\\CSCI49500DPL_Capstone_ML_Project\\Files\\prefix-suffix.txt");
+            string data = File.ReadAllText("C:\\Users\\Anau Naga\\source\\repos\\CSCI49500DPL_Capstone_ML_Project\\Files\\BBC_train_full.csv");
+            Console.Write(data);
+            Console.ReadLine();
+
+        }
+
+        public static string[][] parseDataCSV(string data)
+        {
+            while (true)
+            {
+                
+            }
+
+
+
+
+            return null;
+        }
+
+    }
+
+    
+    public static class Stemming
+    {
+        public static void stemming()
+        {
+
+        }
+
+
+
+
+    }
+
+    public static class Lementization
+    {
+        public static void lemmentization()
+        {
+
+        }
+    }
+
+}
---
+++ Utility/WordTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class WordTree
+    {
+        protected Tree wordTree = new Tree();
+
+
+        public WordTree(string[] words) { 
+            foreach (string word in Words)
+            {
+                wordTree.addWord(word);
+            }
+
+
+        }
+
+
+        public class Tree
+        {
+            Dictionary<char,Tree> branches = new Dictionary<char, Tree>();
+            Tree RootTree = null;
+            char letter;
+
+            //  Adds a word to the word tree
+            //  Returns the leaf containing the last letter of the added word
+            //
+            public Tree addWord(string word)
+            {
+                Tree currTree = this;
+                foreach (char letter in word)
+                {
+                    if (currTree.branches.ContainsKey(letter))
+                    {
+                        currTree = currTree.branches[letter];
+                        currTree.letter = letter;
+                    }
+                    else
+                    {
+                        currTree.branches.Add(letter, new Tree);
+                        currTree.branches[letter].RootTree = currTree;
+                        currTree.branches[letter].RootTree = currTree;
+                        currTree.letter = letter;
+                    }
+                }
+                return currTree;
+            }
+        }
+
+    }
+}
